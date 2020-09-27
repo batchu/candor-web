0450439866a41c49402f152bab32d5d9db7f9e7c
Prashanth Batchu <prashanth@batchu.net>
2020-09-27 11:56:31 -0400

    Created a basic task list

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {Component} from 'react'
+import ReactDom from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Header extends Component{
+
+    render(){
+        return <h1>Task List</h1>
+    }
+}
+
+class Tasks extends Component{
+
+    render(){
+
+        const tasks = ['pay my bills', 'walk the dog', 'do the dishes', 'clean the floor', 'Watch tv', 'make dinner']
+        return <ol>
+           {tasks.map((t,index)=>React.createElement("li",{key:t+index},t))}
+        </ol>
+    }
+}
+
+
+class App extends Component{
+    
+    render(){
+        return <div>
+            <Header />
+            <Tasks />
+        </div>
+     
+    }
+}
+
+ReactDom.render(<App />,document.getElementById("root"))
