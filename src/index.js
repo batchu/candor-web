0d3008b7abb56c6531e8b5182b91fcc33e3d9fcd
saiprasad <36000620+saiprasadsamudrala@users.noreply.github.com>
2020-11-09 12:01:39 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import Main from './Main'
 import './styles/stylesheet.css'
-ReactDom.render(<Main />,document.getElementById("root"))
+ReactDom.render(<App />,document.getElementById("root"))
