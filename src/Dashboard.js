1d077b51e2021a75070f7ddf2a9cdb6f83e29f2a
saiprasadsamudrala <samudralasai1999@protonmail.com>
2020-12-27 15:35:27 +0530

    updated dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,9 +1,18 @@
-import React, {Component} from 'react'
-import IssueList from './IssueList'
+import React from 'react'
+import Issue from './Issue'
 
 class Dashboard extends React.Component{
     render(){
-        return <h1>Dashboard</h1>
+        return <div>
+            <h1>Dashboard</h1>
+            <button type={Image} className="settingIcon">
+            <img src="https://img.icons8.com/fluent-systems-filled/24/000000/settings.png"/>
+            </button>
+            <h3>No issues to display!</h3>
+            <button type={Image} className="newIssue">
+            <img src="https://img.icons8.com/pastel-glyph/64/000000/plus--v1.png"/>
+            </button>
+        </div> 
     }
 }
 export default Dashboard
