70d8bb423ccfe1bb9e33a3d98c5e10bff5c4720a
saiprasadsamudrala <samudralasai1999@protonmail.com>
2020-12-27 15:38:24 +0530

    updated stylesheet

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,7 +5,7 @@ class Dashboard extends React.Component{
     render(){
         return <div>
             <h1>Dashboard</h1>
-            <button type={Image} className="settingIcon">
+            <button type={Image} className="settingsIcon">
             <img src="https://img.icons8.com/fluent-systems-filled/24/000000/settings.png"/>
             </button>
             <h3>No issues to display!</h3>
