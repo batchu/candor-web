4731970fd975ebecfb5cf53dcc692328953ff4f5
Prashanth Batchu <prashanth@batchu.net>
2021-01-03 09:04:34 -0500

    Added router

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import Issue from './Issue'
+import Button from 'react-bootstrap/Button';
 
 class Dashboard extends React.Component{
     render(){
         return <div>
             <h1>Dashboard</h1>
-            <button type={Image} className="settingsIcon">
-            <img src="https://img.icons8.com/fluent-systems-filled/24/000000/settings.png"/>
-            </button>
+            <Button className="glyphicon glyphicon-cogwheel settingsIcon" >
+            
+                </Button>
+            
+
             <h3>No issues to display!</h3>
-            <button type={Image} className="newIssue">
-            <img src="https://img.icons8.com/pastel-glyph/64/000000/plus--v1.png"/>
-            </button>
+            <Button  className="glyphicon glyphicon-circle-empty-plus newIssue" />
+           
+        
         </div> 
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import App from './App'
 import './styles/stylesheet.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDom.render(<App />,document.getElementById("root"))
