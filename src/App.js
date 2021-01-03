efd324330a0185bd0fd191646b6f82f01ab2cf46
Prashanth Batchu <prashanth@batchu.net>
2021-01-03 09:04:34 -0500

    Added router

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React,{Component} from 'react'
 import Login from './Login'
 import Dashboard from './Dashboard'
 import Settings from './Settings'
+import {BrowserRouter as Router,
+Switch,
+Route,
+Link} from 'react-router-dom'
 
 class App extends React.Component{
 
     render(){
-        return <Login />
+        return <Router>
+            <Switch>
+                <Route path="login">
+                    <Login />
+                </Route>
+                <Route path="/dashboard">
+                    <Dashboard />
+                </Route>
+                <Route path="">
+                    <Login />
+                </Route>
+            </Switch>
+        </Router>
     }
 }
 
