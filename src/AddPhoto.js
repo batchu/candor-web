1f1be850eb493973045cf912b3c1b96c2f89a4cd
Prashanth Batchu <prashanth@batchu.net>
2021-01-24 10:18:32 -0500

    Saving changes

---
+++ src/AddPhoto.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {withRouter  } from 'react-router-dom'
+class AddPhoto extends React.Component{
+
+    constructor()
+    {
+        super()
+        this.handleSubmit=this.handleSubmit.bind(this)
+    }
+    handleSubmit(e){
+        e.preventDefault()
+        const link = e.target.elements.link.value
+        const description = e.target.elements.description.value
+        this.props.onAddPhoto(link, description)
+        this.props.history.push("/")
+       
+        
+    }
+    render(){
+        return <div className="form">
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" placeholder ="Link" name="link" />
+                <input type="text" placeholder ="Description" name="description" />
+                <button placeholder="Post">Post</button>
+            </form>
+        </div>
+    }
+}
+export default withRouter(AddPhoto);
