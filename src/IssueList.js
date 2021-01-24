6de40870c945c78ce66fc2b3dee663de7a223002
Prashanth Batchu <prashanth@batchu.net>
2021-01-24 10:49:43 -0500

    CSS updates for IssueList component

--- src/IssueList.js
+++ src/IssueList.js
@@ -1,45 +1,44 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import Issue from './Issue'
 
-class IssueList extends React.Component{
-    render(){
-        return<div>
-        <h1>Dashboard</h1>
-        <button className="settingsIcon">
-         <span className="glyphicon glyphicon-cog"></span>    
-        </button>
-        <label className="switch">
-            <input type="checkbox" />
-            <span className="slider round"></span>
-        </label>
-        <table id= "issueTable">
-            <tr>
-                <th>Summary</th>
-                <th>Company</th>
-                <th>Created</th>
-            </tr>
-            <tr>
-                <td>Unable to add new phone line
-                    <button>
-                    <span class="editIcon" className="glyphicon glyphicon-edit"></span>    
-                    </button>
-                </td>
-                <td>AT&T</td>
-                <td>Today</td>
-            </tr>
-            <tr>
-                <td>Xbox live account is diabled
-                <button>
-                    <span class="editIcon" className="glyphicon glyphicon-edit"></span>    
-                    </button></td>
-                <td>Microsoft</td>
-                <td>2 days ago</td>
-            </tr>
-</table>
-        <button className="newIssue">
-         <span className="glyphicon glyphicon-plus-sign"></span>    
-        </button>
-    </div> 
+class IssueList extends React.Component {
+    render() {
+        return <div>
+            <h1>Dashboard</h1>
+            <button className="settingsIcon">
+                <div className="glyphicon glyphicon-cog"></div>
+            </button>
+            <label className="switch">
+                
+                <div className="slider "></div>
+            </label>
+            <table id="issueTable">
+                <tr>
+                    <th>Summary</th>
+                    <th>Company</th>
+                    <th>Created</th>
+                </tr>
+                <tr>
+                    <td>
+                        <div className="content">Unable to add new phone line</div>
+                        <div className="editIcon">   <button className="glyphicon glyphicon-edit" /></div>
+                    </td>
+                    <td>AT&T</td>
+                    <td>Today</td>
+                </tr>
+                <tr>
+                    <td><div className="content">Xbox live account is diabled</div>
+                        <div className="editIcon">  <button className="glyphicon glyphicon-edit" />
+                            
+                        </div></td>
+                    <td>Microsoft</td>
+                    <td>2 days ago</td>
+                </tr>
+            </table>
+            <button className="newIssue">
+                <div className="glyphicon glyphicon-plus-sign"></div>
+            </button>
+        </div>
     }
 }
 export default IssueList
